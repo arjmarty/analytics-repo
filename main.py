61dd78d1f88bd80509b9f2920e1cd17f6436c07a
arjmarty <annrose0227@gmail.com>
2025-05-30 23:19:06 +0800

    add loading process

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 import os
+import time
 
 
 # Define the base directory (the directory of the current script)
@@ -36,8 +37,17 @@ data = extracted_data.rename(
     }
 )
 
-
 st.set_page_config(layout="wide")
+
+progress_text = "Loading... Please wait."
+my_bar = st.progress(0, text=progress_text)
+
+for percent_complete in range(100):
+    time.sleep(0.01)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+time.sleep(1)
+my_bar.empty()
+
 
 # main header/ title
 st.header(":orange[Airbnb Data Report]")
